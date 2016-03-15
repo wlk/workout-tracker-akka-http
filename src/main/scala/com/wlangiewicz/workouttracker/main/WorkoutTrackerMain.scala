1878aeb77d288d1b8241f0752b1beb180e726045
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-03-15 11:13:52 +0100

    starting api

--- src/main/scala/com/wlangiewicz/workouttracker/main/WorkoutTrackerMain.scala
+++ src/main/scala/com/wlangiewicz/workouttracker/main/WorkoutTrackerMain.scala
@@ -1,5 +1,28 @@
 package com.wlangiewicz.workouttracker.main
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+import com.wlangiewicz.workouttracker.api.Api
+import com.wlangiewicz.workouttracker.dao.{UserDao, WorkoutDao}
+import com.wlangiewicz.workouttracker.services.{ReportingService, UserService, WorkoutService}
+import scala.concurrent.ExecutionContextExecutor
+
+class WorkoutTrackerApi()(override implicit val system: ActorSystem, override implicit val executor: ExecutionContextExecutor) extends Api {
+  val userDao = new UserDao
+  val workoutDao = new WorkoutDao
+  val userService = new UserService(userDao)
+  val workoutService = new WorkoutService(workoutDao)
+  val reportingService = new ReportingService
+}
+
 object WorkoutTrackerMain extends App {
-  println("TODO")
+  implicit val system = ActorSystem("workout-tracker")
+  implicit val dispatcher = system.dispatcher
+
+  val api = new WorkoutTrackerApi()
+
+  implicit val materializer = ActorMaterializer()
+
+  Http().bindAndHandle(api.routes, interface = "0.0.0.0", port = 8080)
 }
